4f72a268c562a32da49887191baf0e38168a2906
wei qi <weiqi@hzdracom.com>
2016-08-16 13:49:24 +0800

    Initial Commit

---
+++ WQImageDisplayView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WQImageDisplayView
+//
+//  Created by 魏琦 on 16/8/16.
+//  Copyright © 2016年 com.drcacom.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WQImageDisplayView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WQImageDisplayView
+//
+//  Created by 魏琦 on 16/8/16.
+//  Copyright © 2016年 com.drcacom.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
